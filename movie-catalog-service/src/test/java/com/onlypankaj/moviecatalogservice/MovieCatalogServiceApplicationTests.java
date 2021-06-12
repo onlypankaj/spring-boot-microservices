a7a410d673041c8d12883210fa1e9edac73c43d7
onlypankaj <onlypankaj@gmail.com>
2021-06-13 00:32:07 +0530

    First Commit blank spring boot project of all 3

---
+++ movie-catalog-service/src/test/java/com/onlypankaj/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlypankaj.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
