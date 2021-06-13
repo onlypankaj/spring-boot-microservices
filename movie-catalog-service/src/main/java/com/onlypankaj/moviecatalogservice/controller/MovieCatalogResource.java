b042e196c02db18d78968534cd377c7f1c8de300
onlypankaj <onlypankaj@gmail.com>
2021-06-13 13:17:06 +0530

    Instead of Resttemplate use WebClient.builder as Reactive Programming

--- movie-catalog-service/src/main/java/com/onlypankaj/moviecatalogservice/controller/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/onlypankaj/moviecatalogservice/controller/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,9 +21,13 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         // Based on Userid call Movie info service to find list of Movie Ids
+
 
         // For Each Movie Id call rating data service to get rating for each
         List<RatingResponse> ratingResponses = Arrays.asList(
@@ -33,7 +38,15 @@ public class MovieCatalogResource {
         //Put them all together
         return ratingResponses.stream().map(rating -> {
 
-            MovieResponse movieResponse = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), MovieResponse.class);
+//            MovieResponse movieResponse = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), MovieResponse.class);
+
+            MovieResponse movieResponse = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(MovieResponse.class)
+                    .block();
+
             return new CatalogItem(movieResponse.getMovieTitle(), "trans", rating.getRating());
         })
                 .collect(Collectors.toList());
