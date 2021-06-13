b5824e16baee402cdb2fff2a3990ec3066a1725f
onlypankaj <onlypankaj@gmail.com>
2021-06-13 12:38:36 +0530

    Calling Resttemplate for each URL refresh is not good Hence create Bean for Resttemplate

--- movie-catalog-service/src/main/java/com/onlypankaj/moviecatalogservice/controller/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/onlypankaj/moviecatalogservice/controller/MovieCatalogResource.java
@@ -3,6 +3,7 @@ package com.onlypankaj.moviecatalogservice.controller;
 import com.onlypankaj.moviecatalogservice.model.CatalogItem;
 import com.onlypankaj.moviecatalogservice.model.MovieResponse;
 import com.onlypankaj.moviecatalogservice.model.RatingResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,12 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         // Based on Userid call Movie info service to find list of Movie Ids
-        RestTemplate movieRestTemplate = new RestTemplate();
-//        RestTemplate restTemplate = new RestTemplate();
-//        MovieResponse movieResponse = restTemplate.getForObject("http://localhost:8082/resource/12", MovieResponse.class);
 
         // For Each Movie Id call rating data service to get rating for each
         List<RatingResponse> ratingResponses = Arrays.asList(
@@ -32,7 +33,7 @@ public class MovieCatalogResource {
         //Put them all together
         return ratingResponses.stream().map(rating -> {
 
-            MovieResponse movieResponse = movieRestTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), MovieResponse.class);
+            MovieResponse movieResponse = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), MovieResponse.class);
             return new CatalogItem(movieResponse.getMovieTitle(), "trans", rating.getRating());
         })
                 .collect(Collectors.toList());
